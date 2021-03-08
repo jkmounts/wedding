2d9eedb131dc366a6ad55e7fa5f898b2e4d5a965
Jestin Mounts <jkmounts@gmail.com>
2021-03-08 10:46:39 -0500

    Added desktop media query

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Oct 23, 2021 00:00:00").getTime();
+var countDownDate = new Date("Oct 23, 2021 15:00:00").getTime();
 var x = setInterval(function() {
     var now = Date.now();
     var distance = countDownDate - now;
